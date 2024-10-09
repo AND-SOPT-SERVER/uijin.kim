f250626ebd94442620b4c09e058dc20a5353f0a6
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2024-10-09 21:11:11 +0900

    [FEAT] : 1차 세미나 과제 필수 구현

---
+++ src/main/java/org/sopt/week1/DiaryService.java
@@ -0,0 +1,26 @@
+package org.sopt.week1;
+
+import java.util.List;
+
+public class DiaryService {
+    private final DiaryRepository diaryRepository = new DiaryRepository();
+
+    void writeDiary(final String body) {
+        final Diary diary = new Diary(null, body);
+
+        diaryRepository.save(diary);
+    }
+
+    List<Diary> getDiaryList() {
+        return diaryRepository.findAll();
+    }
+
+    void deleteDiary(final Long id) {
+        diaryRepository.deleteById(id);
+    }
+
+    void updateDiary(final Long id, final String body) {
+        diaryRepository.updateById(id, body);
+    }
+
+}
